10943929113af530094853862a67530da93ac54b
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-08 22:15:16 +0500

    Shart operatoriga oid masalalar
    Shart operatoriga oid masalalar

---
+++ 02_masala.py
@@ -0,0 +1,5 @@
+n=int(input("Istalgan butun son kiriting n => "))
+if n>0:
+    print(n+1)
+else:
+    print(n-2)
---
+++ 03_masala.py
@@ -0,0 +1,8 @@
+n=int(input("Istalgan butun son kiriting n => "))
+a=10
+if n>0:
+    print(n+1)
+elif n<0:
+    print(n-2)
+else:
+    print(n+a)
